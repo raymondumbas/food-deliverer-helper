9c364767b662d95e6435f406861b93e36d35f0a7
raymondumbas <raymond.umbas@gmail.com>
2024-09-03 22:57:30 -0700

    initialize activeDays

--- Food-Deliverer-Helper/src/main.tsx
+++ Food-Deliverer-Helper/src/main.tsx
@@ -3,6 +3,13 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+//Initialize localStorage
+if(!localStorage.getItem("activeDays")){
+
+  localStorage.setItem("activeDays",JSON.stringify([]))
+  
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />

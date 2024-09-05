ecac964ec96c6893a5382298406ca2aedbc7f6ba
raymondumbas <raymond.umbas@gmail.com>
2024-09-05 00:48:21 -0700

    Implemented functions for getting certain useful dates

--- Food-Deliverer-Helper/src/utilities.tsx
+++ Food-Deliverer-Helper/src/utilities.tsx
@@ -1,4 +1,4 @@
-function findStartOfWeek(inputDate : Date): Date{
+function getWeekStart(inputDate : Date): Date{
 
     const currentDate: Date = inputDate;
 
@@ -8,4 +8,37 @@ function findStartOfWeek(inputDate : Date): Date{
 
     return currentDate;
 }
-export default findStartOfWeek;
+
+function getWeekDates(inputDate: Date): Date[]{
+
+    const currentDate: Date = getWeekStart(inputDate);
+    let weekDates : Date[] = []
+
+    for(let i = 0; i < 7; i++){
+        
+        weekDates.push(currentDate);
+        currentDate.setDate(currentDate.getDate() + 1);
+
+    }
+
+    return weekDates;
+}
+
+function getMonthlyWeekStarts(inputDate:Date): Date[]{
+
+    let currentWeekStart: Date = getWeekStart(inputDate);
+    let monthlyWeekStarts: Date[] = [];
+
+    // Loop through each week of the month to get the start day for each week
+    for(let i = 0; i < 4; i++){
+      
+        monthlyWeekStarts.push(new Date(currentWeekStart));
+        
+        currentWeekStart.setDate(currentWeekStart.getDate() - 7);
+       
+    }
+
+    return monthlyWeekStarts;
+
+}
+export {getWeekStart, getWeekDates, getMonthlyWeekStarts};
